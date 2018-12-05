d429db7eeab6fc5e842fa9b95d976733f8a5953c
justen <justenc94@gmail.com>
2018-12-05 17:23:07 -0600

    pig latin program prototype completed - todo: validate input, account for punctuation, and create function from script

--- pigLatin.py
+++ pigLatin.py
@@ -1,7 +1,27 @@
 #changes user input to pig latin and return a string
 
-userInput = list(input("Enter a string: "))
+#TODO: validate input, allow for punctuation, convert script into functions
 
-str1 = ''.join(userInput)
+userInput = input("Enter a string to convert to pig latin: ")
 
-print(str1)
+list1 = userInput.split()
+i = 0
+c = len(list1)
+pTempList = list()
+
+for i in range(c):
+    tempList = list(list1[i])
+    tempList.append(tempList[0])
+    tempList.remove(tempList[0])
+    tempList.append("-ay")
+    pList = tempList.copy()
+    str1 = ''.join(pList)
+
+    while i in range(c):
+        pTempList.append(str1)
+        break
+    i = i + 1
+
+pString = ' '.join(pTempList)
+
+print("Pig latin version:\n" + pString)
